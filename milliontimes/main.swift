923b0067ff51a5a3eb864831cb15900dbdd4a6a7
Manny Yusuf <manolovayusuf@pursuit.org>
2018-10-05 16:45:17 -0400

    I'm Stressed

---
+++ milliontimes/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  milliontimes
+//
+//  Created by Manny Yusuf on 10/5/18.
+//  Copyright © 2018 Manny Yusuf. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
